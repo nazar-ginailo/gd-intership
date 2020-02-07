7959e0c8575186c879d8c371c41f5023e076a481
Nazar Ginailo <nazarginailo@gmail.com>
2020-02-07 14:02:16 +0100

    Update task 13

--- tasks/13_largest-of-four/largest-of-four.js
+++ tasks/13_largest-of-four/largest-of-four.js
@@ -1,15 +1,3 @@
 export function largestOfFour(arr) {
   return arr.map((subArr) => Math.max(...subArr));
 }
-
-// let result = [];
-//
-// arr.forEach((subArr) => {
-//   if (subArr.length === 0) {
-//     throw new Error('Sub array is empty.');
-//   } else {
-//     result.push(Math.max(...subArr));
-//   }
-// });
-// return result;
-
